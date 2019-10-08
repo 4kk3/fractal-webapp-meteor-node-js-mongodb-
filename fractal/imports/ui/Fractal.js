139e1e595c947ec4fc417ed5536354abc6ee3cff
4kk3 <axelvilhelmtuomi7@gmail.com>
2019-10-08 12:45:25 +0300

    Sucessfully generated a fractal!

--- fractal/imports/ui/Fractal.js
+++ fractal/imports/ui/Fractal.js
@@ -2,49 +2,51 @@ import React, { Component } from 'react';
 
 // Task component - represents a single todo item
 export default class Fractal extends Component {
-	generateCanvas() {
+	 componentDidMount() {
 			//Instantiate Canvas
-                        const canvas = this.refs.canvas;
-                        const ctx = canvas.getContext("2d");
-			canvas.width = 600;
-			canvas.height = 600;
+                        var canvas = this.refs.canvas;
+                        var ctx = canvas.getContext("2d");
+			canvas.width = 500;
+			canvas.height = 500;
 			
 			//Draw Canvas
-			generateMandelbrot(canvas, ctx);
+			this.generateMandelbrot(canvas, ctx);
 	}
 	//Mandelbrot
 	generateMandelbrot(canvas, ctx) {
-		var magnificationFactor = 600;
-                var panX = 0;
-                var panY = 0;
+		var magnificationFactor = 2900;
+		var panX = 0.7;
+		var panY = 0.6;
                 for(var x = 0; x < canvas.width; x++) {
                 	for(var y = 0; y < canvas.height; y++) {
-                        	var belongsToSet = checkIfBelongsToMandelbrotSet(x/magnificationFactor - panX, y/magnificationFactor - panY);
-                                if(belongsToSet) {
-                                	ctx.fillRect(x, y, 1, 1); // Draw a black pixel
-                                }
+                        	var belongsToSet = this.checkIfBelongsToMandelbrotSet(x/magnificationFactor - panX, y/magnificationFactor - panY);
+                                if(belongsToSet == 0) {
+    					ctx.fillStyle = '#000';
+    					ctx.fillRect(x,y, 1,1); // Draw a black pixel
+				} else {
+    					ctx.fillStyle = 'hsl(0, 100%, ' + belongsToSet + '%)';
+    					ctx.fillRect(x,y, 1,1); // Draw a colorful pixel
+				}
                         }
 		}
 	}
 
-	checkIfBelongsToMandelbrotSet(x, y) {
-		var realComponentOfResult = x;
-		var imaginaryComponentOfResult = y;
+	checkIfBelongsToMandelbrotSet(x,y) {
+    		var realComponentOfResult = x;
+    		var imaginaryComponentOfResult = y;
+    		var maxIterations = 100;
+    		for(var i = 0; i < maxIterations; i++) {
+         		var tempRealComponent = realComponentOfResult * realComponentOfResult - imaginaryComponentOfResult * imaginaryComponentOfResult + x;
+         		var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult + y;
+         		realComponentOfResult = tempRealComponent;
+         		imaginaryComponentOfResult = tempImaginaryComponent;
 
-		for(var i = 0; i < 10; i++) {
-     			// Calculate the real and imaginary components of the result
-     			// separately
-     			var tempRealComponent = realComponentOfResult * realComponentOfResult - imaginaryComponentOfResult * imaginaryComponentOfResult + x;
-     			var tempImaginaryComponent = 2 * realComponentOfResult * imaginaryComponentOfResult + y;
-     			realComponentOfResult = tempRealComponent;
-     			imaginaryComponentOfResult = tempImaginaryComponent;
-		}	
-
-		if (realComponentOfResult * imaginaryComponentOfResult < 5)
-    			return true; // In the Mandelbrot set
-
-		return false; // Not in the set
-	}
+         		// Return a number as a percentage
+         		if(realComponentOfResult * imaginaryComponentOfResult > 5) 
+            			return (i/maxIterations * 100);
+    		}
+    		return 0;   // Return zero if in set        
+	}       
 
 	//Mandelbrot
 	render() {
@@ -53,3 +55,4 @@ export default class Fractal extends Component {
     		);
   	}
 }
+
