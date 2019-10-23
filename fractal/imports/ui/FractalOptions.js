02da57a682a2c03fc65fa6897968d927e516d14c
bob <axelvilhelmtuomi7@gmail.com>
2019-10-23 09:55:37 +0300

    working with fractal

--- fractal/imports/ui/FractalOptions.js
+++ fractal/imports/ui/FractalOptions.js
@@ -3,23 +3,21 @@ import React, { Component } from 'react';
 import Fractal from './Fractal.js';
 
 export default class FractalOptions extends Component {
+	regenerate(props){
+			
+	}
+	
         render() {
                 return (
-			<Fractal/>
-                        <form>
-				Iterations:<br>
-				<input type = "number" name = "iterations"><br>
-				Resolution:<br>
-				<select>
-  					<option value="volvo">Volvo</option>
-  					<option value="saab">Saab</option>
-  					<option value="mercedes">Mercedes</option>
-  					<option value="audi">Audi</option>
-				</select><br> 
-				Color:<br>
-				<span class="spectrum"></span><br>
-				<input type="range" min="0" max="300" step="1"><br>
-			</form>
+			<div classname = "container">
+				<Fractal/ >
+				<form>
+					Resolution:<br/>
+					<input type="number" name = "x" placeholder = "x"/>
+					<input type="number" name = "y" placeholder = "y"/>
+				</form>
+				<button type = "button" onClick={() => this.regenerate}>Regenerate</button>
+			</div>
                 );
         }
 }
