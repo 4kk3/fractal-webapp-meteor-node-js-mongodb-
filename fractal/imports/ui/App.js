1a71a7692c192bbbbf88f5c6a2995efcc0b9e356
bob <axelvilhelmtuomi7@gmail.com>
2019-11-26 14:15:23 +0200

    working on database

--- fractal/imports/ui/App.js
+++ fractal/imports/ui/App.js
@@ -12,21 +12,16 @@ class App extends Component {
                 };
 	}
 	componentDidUpdate() {
-                var listexceptionvalue;
+		console.log(this.props.databasevalues.length)
                 var i;
                 for (i = 0; i < this.props.databasevalues.length; i++) {
-                        if (i == this.props.databasevalues.length - 1) {
-                                listexceptionvalue = 0;
-                        }
-                        else {
-                                listexceptionvalue = 1;
-                        }
-                        if (this.state.listoffractalpresets.includes(Object.values(this.props.databasevalues[i])[listexceptionvalue]) != true) {
+			console.log(Object.values(this.props.databasevalues[i])[1])
+			console.log(this.props.databasevalues[i])
+                        if (this.state.listoffractalpresets.includes(Object.values(this.props.databasevalues[i])[1]) != true) {
                                 this.setState(prevState => ({
-                                        listoffractalpresets: [...prevState.listoffractalpresets, Object.values(this.props.databasevalues[i])[listexceptionvalue]]
+                                        listoffractalpresets: [...prevState.listoffractalpresets, Object.values(this.props.databasevalues[i])[1]]
                                 }))
                         }
-
                 }
         }
 	render() {
