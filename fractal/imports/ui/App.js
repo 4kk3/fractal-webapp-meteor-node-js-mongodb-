0de0d5eeb60b7880e1524c9067d0ec3a84537e91
bob <axelvilhelmtuomi7@gmail.com>
2019-11-27 09:46:13 +0200

    working on database

--- fractal/imports/ui/App.js
+++ fractal/imports/ui/App.js
@@ -12,17 +12,22 @@ class App extends Component {
                 };
 	}
 	componentDidUpdate() {
-		console.log(this.props.databasevalues.length)
+		if (this.props.trackerdone == true) {
+			
+				
+		}
+	}
+	savedatabasetostate() {
                 var i;
-                for (i = 0; i < this.props.databasevalues.length; i++) {
-			console.log(Object.values(this.props.databasevalues[i])[1])
-			console.log(this.props.databasevalues[i])
+                for (i = 0; i < this.props.databasevalues.length - 1; i++) {
                         if (this.state.listoffractalpresets.includes(Object.values(this.props.databasevalues[i])[1]) != true) {
                                 this.setState(prevState => ({
                                         listoffractalpresets: [...prevState.listoffractalpresets, Object.values(this.props.databasevalues[i])[1]]
                                 }))
                         }
                 }
+		console.log(this.props.databasevalues);
+		console.log(this.state.listoffractalpresets);
         }
 	render() {
     		return (
@@ -39,5 +44,6 @@ class App extends Component {
 export default withTracker(() => {
         return {
 		databasevalues: SavedFractals.find().fetch(),
+		trackerdone: true,
 	};
 })(App);
