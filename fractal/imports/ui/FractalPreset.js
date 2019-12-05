07152475fd9fb0f97a2dff3bdf4fe2d4714a0335
bob <axelvilhelmtuomi7@gmail.com>
2019-12-05 11:33:13 +0200

    dfsf

--- fractal/imports/ui/FractalPreset.js
+++ fractal/imports/ui/FractalPreset.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-
-// App component - represents the whole app
+import {SavedFractals} from '../api/savedfractals.js';
 
 export default class FractalPreset extends Component {
-         savetodatabase = () => {
-                const {fractalvalues} = this.state;
-                SavedFractals.insert({
-                        fractalvalues,
-                        createdAt: new Date(),
+
+        savetodatabase = () => {
+                SavedFractals.update(this.props.task._id{
+         		$set: { presetgenerated: true},
                 });     
         }
+
 	render() {
                 return (
                         <div className="container">
-				<h2 onClick = {()=>this.props.triggerParentUpdate(this.props.fractalvalues)}>{this.props.fractalvalues[0]}</h2>
+				<h2 onClick = this.savetodatabase>{this.props.fractalvalues[0]}</h2>
                         </div>
                 );
         }
