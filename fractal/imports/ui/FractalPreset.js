a72855220d7f1e06f578194d2a069d64b26d4c40
4kk3 <axelvilhelmtuomi7@gmail.com>
2019-12-16 14:14:56 +0200

    got the databse working finally!

--- fractal/imports/ui/FractalPreset.js
+++ fractal/imports/ui/FractalPreset.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react';
 import {SavedFractals} from '../api/savedfractals.js';
 
 export default class FractalPreset extends Component {
-
+	constructor(props) {
+                super(props);
+                //this.renderpreset = this.renderpreset.bind(this);
+                this.savetodatabase = this.savetodatabase.bind(this);
+        }
         savetodatabase = () => {
                 SavedFractals.update(this.props.fractalvalues[0], {
          		$set: { fractalvalues: [presetgenerated: true]},
                 });     
         }
-
+	test() {
+		console.log("dggdsf");
+	}
 	render() {
 		console.log("loL");
                 return (
                         <div className="container">
-				<h2 onClick = {this.savetodatabase} onClick = {this.props.triggerParentUpdate}>{this.props.fractalvalues[1][0]}</h2>
+				<a onClick = {this.test} onClick = {this.props.triggerParentUpdate}>{this.props.fractalvalues[1][0]}</a>
                         </div>
                 );
         }
--- fractal/imports/ui/Share.js
+++ fractal/imports/ui/Share.js
@@ -9,36 +9,39 @@ class Share extends Component {
 		this.state = {
 			presetcount: 0	
 		};
-		this.renderpreset = this.renderpreset.bind(this);
+		//this.renderpreset = this.renderpreset.bind(this);
+		this.sharesendup = this.sharesendup.bind(this);
 	}
 
-	componentDidUpdate() {
-		if (this.state.presetcount < this.props.savedfractals.length) {
-			var i = this.state.presetcount
-			while (i < this.props.savedfractals.length) {
-				this.renderpreset(Object.values(this.props.savedfractals[i]));
-				i++;
-			}
-			this.setState({presetcount: i});
-		}
-	}
 	sharesendup() {
 		this.props.triggerParentUpdate;
 	}
 
-	renderpreset(fractalvalues) {
-		return <FractalPreset fractalvalues = {fractalvalues} triggerParentUpdate = {this.sharesendup} />;
-	}
+	//renderpreset(fractalvalues) {
+	//	return <FractalPreset fractalvalues = {fractalvalues} triggerParentUpdate = {this.sharesendup} />;
+	//}
 
         render() {
+		//if (this.state.presetcount < this.props.savedfractals.length) {
+                        //var i = this.state.presetcount
+                       // while (i < this.props.savedfractals.length) {
+                      //          this.renderpreset(Object.values(this.props.savedfractals[i]));
+                    //            i++;
+                  //      }
+                //        this.setState({presetcount: i});
+                //}
                 return (
                         <div className="container">
+				{this.props.savedfractals.map(function(fractalvalues, i) {
+					return <FractalPreset fractalvalues = {Object.values(fractalvalues)} key = {i}/>
+				})}
                         </div>
                 );
         }
 }
 
 export default withTracker(() => {
+	console.log(SavedFractals.find().fetch());
         return {
 		savedfractals: SavedFractals.find().fetch(),
 	};
